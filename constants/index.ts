d49cdbccfd66045579cbbba262edc402e1e14866
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-04-04 03:37:41 +0600

    New zoom app start

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+    {
+        label: "Home",
+        route: "/",
+        imaUrl: "icons/Home.svg",
+    },
+    {
+        label: "Upcoming",
+        route: "/upcoming",
+        imaUrl: "icons/upcoming.svg",
+    },
+    {
+        label: "Previous",
+        route: "/previous",
+        imaUrl: "icons/previous.svg",
+    },
+    {
+        label: "Recordings",
+        route: "/recordings",
+        imaUrl: "icons/Video.svg",
+    },
+    {
+        label: "Personal Room",
+        route: "/personal-room",
+        imaUrl: "icons/add-personal.svg",
+    },
+]
